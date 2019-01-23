18910dca3ba36cf178f3c87842ef25bf39a11dba
ming <liangming@thinkyun.cn>
2019-01-23 17:08:52 +0800

    bottle

--- bottletest.py
+++ bottletest.py
@@ -14,19 +14,53 @@
 import bottle
 import json
 
+from bottle import request,run,route
+
+import os
+
 #定义图片路径
+import os
+
 images_path = './images'
-@bottle.route('/images/<filename:re:.*\.*>')
+@route('/images/<filename:re:.*\.*>')
 def server_static(filename):
     return bottle.static_file(filename, root=images_path, download=True)
 
-@bottle.route("/getJson", methods=['GET', 'POST'])
+upload_path = './upload'
+@route('/uploadfile/<filename:re:.*\.*>')
+def uploadfile_static(filename):
+    return bottle.static_file(filename, root=upload_path)
+
+@route("/getJson", methods=['GET', 'POST'])
 def getJson():
-    formid = bottle.request.query.id
-    pageid = bottle.request.query.page or '1'
+    formid = request.query.id
+    pageid = request.query.page or '1'
     print(formid, pageid)
-    list = ['fdsfds', 'fdsfdsfds']
-    dict = {"response": list}
+    list = os.listdir('./upload')
+    dict = {"data": list, "code":0, "msg":'success'}
     return json.dumps(dict)
 
-bottle.run(host='0.0.0.0', port=9999)
+@route('/uploadDetail', method='GET')
+def upload():
+    return '''<form action="/upload" method="post" enctype="multipart/form-data">
+                  filename:      <input type="text" name="fileName" />
+                  Select a file: <input type="file" name="upload" />
+                  <input type="submit" value="Start upload" />
+             </form>'''
+
+@route('/upload', method='POST')
+def do_upload():
+    file_name   = request.forms.get('fileName')
+    print(file_name)
+    upload     = request.files.get('upload')
+    if upload:
+        name, ext = os.path.splitext(upload.filename)
+        print(name, ext)
+        save_path = 'upload/'+ file_name
+        print(save_path)
+        upload.save(save_path) # appends upload.filename automatically
+        return 'OK'
+    else:
+        return '请选择文件'
+
+run(host='0.0.0.0', port=9999)
